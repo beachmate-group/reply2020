f81499100fc7c5663ed72a7749d38b4473647746
bonfry <bonfry98@live.it>
2020-03-12 17:42:05 +0100

    First

---
+++ src/com/esamejava/ProjectManager.java
@@ -0,0 +1,13 @@
+package com.esamejava;
+
+public class ProjectManager {
+    private int bonus;
+
+    public int getBouns() {
+        return bonus;
+    }
+
+    public void setBouns(int bouns) {
+        this.bonus = bouns;
+    }
+}
